a85cee0365e539bee35fcc0dfcc051ff398a4a92
Hun Yao Chong <40569873+ZephyRemix@users.noreply.github.com>
2024-11-26 15:08:58 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,1 @@
-import { renderHomePage } from "./home.js";
-import { renderMenuPage } from "./menu.js";
 
-const navBar = document.querySelector('ul');
-let contentDiv = document.querySelector('#content');
-
-renderHomePage();
-
-navBar.addEventListener("click", e => {
-    contentDiv.innerHTML = '';
-
-    if (e.target.getAttribute("id") === "home-btn") {
-        renderHomePage();
-    } 
-    else if (e.target.getAttribute("id") === "menu-btn") {
-        renderMenuPage();
-    } 
-});
